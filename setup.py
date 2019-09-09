2e4dbe1104477e284e0d53373997dd7a06be877d
Juan Diaz <furiosojuan0@gmail.com>
2019-09-09 16:15:38 -0500

    Se actualiza setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'wssedian2',
   packages = ['wssedian2'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'Firmador de las peticiones SOAP a la DIAN con version de prevalidacion',
   author = 'FuriosoJack(Juan Diaz)',
   author_email = 'iam@furiosojack.com',
   url = 'https://github.com/FuriosoJack/pyWSSE-Dian2', # use the URL to the github repo
-  download_url = 'https://github.com/FuriosoJack/pyWSSE-Dian2/tarball/0.1',
-  keywords = ['testing', 'logging', 'example'],
+  download_url = 'https://github.com/FuriosoJack/pyWSSE-Dian2/tarball/0.1.1',
+  keywords = ['firma', 'dian', 'facturacion','electronica','ubl','soap'],
   classifiers = [],
 )
